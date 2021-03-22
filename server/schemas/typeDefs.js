3dffc7730443a9401fd479e9faff4d5b8c69c3a2
RickySulli <71475646+RickySulli@users.noreply.github.com>
2021-03-21 22:34:53 -0500

    error   is not a valid type within the array

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,47 +1,44 @@
-cosnt { gql } = require('apollo-server-express');
+const { gql } = require('apollo-server-express');
 
+// create our typeDefs
 const typeDefs = gql`
-    type User {
-        _id: ID
-        username: String
-        email: String
-        bookCount: Int
-        savedBooks: [Book]
-    }
-    type Book {
-        bookId: String
-        authors: [String]
-        description: String
-        title: String
-        image: String
-        link: String
-    }
-    input BookInput {
-        bookId: ID
-        authors: [String!]
-        description: String!
-        title: String!
-        image: String!
-        link: String!
+  type Auth {
+      token: ID!
+      user: User
+  }
+  type Book {
+      bookId: ID
+      authors: [String]
+      description: String
+      title: String
+      image: String
+      link: String 
+  }
+  type User {
+      _id: ID
+      username: String
+      email: String
+      bookCount: Int
+      savedBooks: [Book]
+  }
+  input bookInput {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Query {
+      me: User
+  }
+  type Mutation {
+      login(email: String!, password: String!): Auth
+      addUser(username: String!, email: String!, password: String!): Auth
+      saveBook(input: bookInput): User
+      removeBook(bookId: ID!): User
+  }
+`;
 
-    }
-
-    type Query {
-        me: User
-        user(username:String!): User
-        savedBooks(username:String): [Book]
-    }
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username:String!, email: String!, password: String!): Auth
-        saveBook(bookData: BookInput): User
-        removeBook(bookId:ID!): User
-    }
-    type Auth {
-        token: ID!
-        user: User
-    }
-`
-
-
+// export the typeDefs
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-const {typeDefs, resolvers} = require('./')
-const { AuthenticationError, ApolloServer } = require('apollo-server-express');
+const {typeDefs, resolvers} = require('./schemas')
 const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -26,8 +26,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(` Now listening on localhost:${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
